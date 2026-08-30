c8f40ea9dd12e44ebfc0196f46e3f109862ed227
Saran R <saran12stu@gmail.com>
2026-08-30 21:30:29 +0530

    Add files via upload

---
+++ 06_Numbers.py
@@ -0,0 +1,18 @@
+a = 12
+b = 12.5
+c = 12j
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(str(a))
+print(int(a))
+print(complex(a))
+
+import random
+print(random.randrange(1,100))
