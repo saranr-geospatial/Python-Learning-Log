4a7b0547058afcffdf94c025f2d708d560012934
Saran R <saran12stu@gmail.com>
2026-08-30 15:47:33 +0530

    Add files via upload

---
+++ Syntax.py
@@ -0,0 +1,28 @@
+# It is a Correct syntax (Identation)
+
+if 5 > 2:
+    print("Five is greater than two")
+
+# Syntax is wrong
+
+'''
+if 5 > 2:
+print("Five is greater than two")
+'''
+
+# Variables
+
+x = 20 
+y = "Hello World"
+
+# Comments
+
+# This is a Comment 
+
+'''This Also a Comment'''
+
+# Statements
+
+print("Python is Fun")
+
+print("Hi My Name is Saran") ; print("My Aim is to get into ISRO")
